ce1ca3bd77ddeef2b80e3a886e99250692f04049
home-pmec <home.pmec@gmail.com>
2023-08-05 15:38:52 -0300

    feat(albot-description): add aws launch

---
+++ launch/aws_albot.launch.py
@@ -0,0 +1,62 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch_ros.substitutions import FindPackageShare
+
+
+
+
+def generate_launch_description():
+
+    package_name='albot-description'
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    world = LaunchConfiguration('world')
+
+    declare_world_cmd = DeclareLaunchArgument(
+        'world',
+        default_value=PathJoinSubstitution([
+            FindPackageShare('aws_robomaker_small_warehouse_world'),
+            'worlds',
+            'small_warehouse.sdf'
+        ]),
+        description='Full path to world model file to load'
+    )
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+
+                launch_arguments={
+                    'world': world}.items()
+             )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'albot_robot'],
+                        output='screen')
+
+
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        declare_world_cmd,
+        gazebo,
+        spawn_entity,
+    ])
